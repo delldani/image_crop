264f7e060a0f09d538b1f3dc1a80c6a85a490022
gordongecco <delldani001@gmail.com>
2020-02-21 13:56:22 +0100

    2.21

--- cypress/integration/examples/sample.spec.js
+++ cypress/integration/examples/sample.spec.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "@testing-library/cypress/add-commands";
 
-/// <reference types="cypress" />
-
 describe("My First Test", function() {
   it("Does  much!", function() {
     expect(true).to.equal(true);
@@ -11,8 +9,4 @@ describe("My First Test", function() {
     cy.get("[data-testid=input]").click();
     cy.contains("kép helye");
   });
-
-  // it("react", function() {
-  //   cy.mount(<div>asasas</div>);
-  // });
 });
